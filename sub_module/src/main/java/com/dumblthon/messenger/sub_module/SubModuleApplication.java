86cff84adad0ff21ad003a5f74bcb8b8d3675323
Sveta Kometa <tsar.morei@gmail.com>
2020-07-02 09:56:43 +0300

    Preparatory commit. Add common module, sub_module

---
+++ sub_module/src/main/java/com/dumblthon/messenger/sub_module/SubModuleApplication.java
@@ -0,0 +1,13 @@
+package com.dumblthon.messenger.sub_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubModuleApplication.class, args);
+	}
+
+}
---
+++ sub_module/src/test/java/com/dumblthon/messenger/sub_module/SubModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dumblthon.messenger.sub_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
