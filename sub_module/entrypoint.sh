d1646c27d6a467cf990f326eb5ffc0cf007b962b
ICndel <julia7work@gmail.com>
2020-07-22 22:52:05 +0300

    #8 docker compose with postgres & mongo

--- sub_module/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Postgres container entry point
-#/bin/bash /usr/local/bin/docker-entrypoint.sh
-
-su - postgres -c "/usr/lib/postgresql/12/bin/postgres -D /var/lib/postgresql/12/main -c config_file=/etc/postgresql/12/main/postgresql.conf"
-
-# Run app
-java -jar -Dapplication.logpath="${LOG_PATH}" -Xms512M -Xmx4096M /opt/app/app.jar
